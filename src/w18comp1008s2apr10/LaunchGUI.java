39fcd1d14a9f82ecc1dda0b2129ffbc5b5efd907
JaretWright <hkkA223>
2018-04-10 14:54:41 -0400

    Initial Upload

---
+++ src/w18comp1008s2apr10/LaunchGUI.java
@@ -0,0 +1,31 @@
+package w18comp1008s2apr10;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author JWright
+ */
+public class LaunchGUI extends Application
+{
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("MyNameView.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("This is me!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+}
---
+++ src/w18comp1008s2apr10/W18COMP1008S2Apr10.java
@@ -0,0 +1,18 @@
+package w18comp1008s2apr10;
+
+/**
+ *
+ * @author JWright
+ */
+public class W18COMP1008S2Apr10
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
